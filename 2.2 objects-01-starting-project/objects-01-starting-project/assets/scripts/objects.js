07f656b22191f4f95a5e31bfb064f881f51f590b
zkkamir <zkkamir@gmail.com>
2021-07-14 13:20:59 +0430

    End of S9E8.

--- 2.2 objects-01-starting-project/objects-01-starting-project/assets/scripts/objects.js
+++ 2.2 objects-01-starting-project/objects-01-starting-project/assets/scripts/objects.js
@@ -3,13 +3,17 @@ const movieList = document.getElementById("movie-list");
 movieList.style.backgroundColor = "red";
 movieList.style.display = "block";
 
+const userChosenKeyName = "level";
+
 let person = {
     "first name": "amir",
     age: 30,
     hobbies: ["Sports", "Cooking"],
+    [userChosenKeyName]: '...',
     greet: function () {
         alert("Hi there!");
-    }
+    },
+    1.5: "hello"
 };
 
 // person.age = 23;
@@ -18,6 +22,10 @@ delete person.age;
 // person.age = null;
 person.isAdmin = true;
 
-console.log(person["first name"]);
+const keyName = "first name";
+
+console.log(person[keyName]);
+console.log(person[1.5]);
+console.log(person);
 
 // person.greet();
