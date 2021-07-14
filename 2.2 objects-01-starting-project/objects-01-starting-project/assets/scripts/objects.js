240048e85959535daebbf6b0c9527b53ebb1034c
zkkamir <zkkamir@gmail.com>
2021-07-14 14:49:39 +0430

    Added search functionality.

--- 2.2 objects-01-starting-project/objects-01-starting-project/assets/scripts/objects.js
+++ 2.2 objects-01-starting-project/objects-01-starting-project/assets/scripts/objects.js
@@ -3,7 +3,7 @@ const searchButton = document.getElementById("search-btn");
 
 const movies = [];
 
-const renderMovies = () => {
+const renderMovies = (filter = '') => {
     const movieList = document.getElementById("movie-list");
 
     if (movies.length === 0) {
@@ -14,7 +14,11 @@ const renderMovies = () => {
     }
     movieList.innerHTML = "";
 
-    movies.forEach((movie) => {
+    const filteredMovies = !filter ? movies : movies.filter(
+        movie => movie.info.title.includes(filter)
+    )
+
+    filteredMovies.forEach((movie) => {
         const movieEl = document.createElement("li");
         let text = movie.info.title + " - ";
         for (const key in movie.info) {
@@ -51,4 +55,10 @@ const addMovieHandler = () => {
     renderMovies();
 };
 
+const searchMovieHandler = () => {
+    const filterTerm = document.getElementById("filter-title").value;
+    renderMovies(filterTerm);
+}
+
 addMovieButton.addEventListener("click", addMovieHandler);
+searchButton.addEventListener("click", searchMovieHandler);
