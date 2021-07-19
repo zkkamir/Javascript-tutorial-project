0ede3de174facefdf276220ad98d4f01aabfc7c6
zkkamir <zkkamir@gmail.com>
2021-07-19 15:16:30 +0430

    used the 'this' keyword.

--- 2.2 objects-01-starting-project/objects-01-starting-project/assets/scripts/objects.js
+++ 2.2 objects-01-starting-project/objects-01-starting-project/assets/scripts/objects.js
@@ -22,8 +22,9 @@ const renderMovies = (filter = '') => {
         const movieEl = document.createElement("li");
         const { info, ...otherProps } = movie;
         console.log(otherProps);
-        const { title: movieTitle } = info;
-        let text = movieTitle + " - ";
+        // const { title: movieTitle } = info;
+        // const { getFormatedTitle } = movie;
+        let text = movie.getFormatedTitle() + " - ";
         for (const key in info) {
             if (key !== "title") {
                 text = text + `${key}: ${info[key]}`;
@@ -51,7 +52,10 @@ const addMovieHandler = () => {
             title,
             [extraName]: extraValue,
         },
-        id: Math.random()
+        id: Math.random().toString(),
+        getFormatedTitle: function () {
+            return this.info.title.toUpperCase();
+        }
     };
 
     movies.push(newMovie);
